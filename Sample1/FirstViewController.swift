0e16e0a762f6f02eab40a868216f3ca09cba9912
18rd164 <masa.miura1202@i.softbank.jp>
2020-06-01 14:25:22 +0900

    テスト1

---
+++ Sample1/FirstViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FirstViewController.swift
+//  Sample1
+//
+//  Created by masa.miura on 2020/05/25.
+//  Copyright © 2020 miura. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //背景設定
+        self.view.addBackground(name: "backgroundimage.jpg")
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
